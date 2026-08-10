7f722f3c3e7b54041bb8073571816d8726ef1eef
22akmal <akmalsyam67@gmail.com>
2026-08-10 11:08:32 +0700

    Finished exercise 1.7 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,6 +12,47 @@ const Paragraph = (props) => {
   )
 }
 
+const Sum = (props) => {
+  const sum = props.feedback[0] + props.feedback[1] + props.feedback[2]
+
+  return (
+    <p>all {sum}</p>
+  )
+}
+
+const Average = (props) => {
+  const sum = props.feedback[0] + props.feedback[1] + props.feedback[2]
+  const goodFeedback = props.feedback[0]
+  const neutralFeedback = props.feedback[1] * 0
+  const badFeedback = props.feedback[2] * -1
+  const average = (goodFeedback + neutralFeedback + badFeedback)/sum
+
+  if (!Number(average)){
+    return (
+      <p>average 0</p>
+    )
+  } else {
+    return (
+      <p>average {average}</p>
+    )
+  }
+}
+
+const PositvePct = (props) => {
+  const sum = props.feedback[0] + props.feedback[1] + props.feedback[2]
+  const percentage = (props.feedback[0] / sum) * 100
+
+  if (!Number(percentage)){
+    return (
+      <p>positive 0</p>
+    )
+  } else {
+    return (
+      <p>positive {percentage} %</p>
+    )
+  }
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -27,6 +68,9 @@ const App = () => {
       <Paragraph text={"good"} number={good}/>
       <Paragraph text={"neutral"} number={neutral}/>
       <Paragraph text={"bad"} number={bad}/>
+      <Sum feedback={[good, neutral, bad]}/>
+      <Average feedback={[good, neutral, bad]}/>
+      <PositvePct feedback={[good, neutral, bad]}/>
     </div>
   )
 }
