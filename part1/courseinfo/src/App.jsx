4f2e76dcd8f489c05f3ca65d297afea479c31b97
22akmal <akmalsyam67@gmail.com>
2026-08-11 14:16:09 +0700

    Finished exercise 2.1 courseinfo step 6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,61 +1,54 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
+const Header = (props) => <h1>{props.course}</h1>
 
-const Part = (props) => {
-  console.log(props)
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-}
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
 
-const Content = (props) => {
-  console.log(props)
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = (props) => {
   return (
     <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
+      <Header course={props.course.name}/>
+      <Content parts={props.course.parts}/>
     </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return <Course course={course}/>
 }
 
 export default App
