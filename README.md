6e31433114c25096f27d85412e1a0a18c0c8bf92
22akmal <akmalsyam67@gmail.com>
2026-09-04 10:12:49 +0700

    new version

---
+++ README.md
@@ -0,0 +1,1 @@
+https://phonebookbackend-kappa.vercel.app/
---
+++ index.js
@@ -0,0 +1,134 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const Person = require('./models/person')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('dist'))
+
+morgan.token('body', (request, response) =>
+  JSON.stringify(request.body)
+)
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(person => {
+      response.json(person)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response) => {
+  const requestTime = new Date()
+
+  Person.find({})
+    .then(persons => {
+      response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${requestTime}</p>
+    `)
+    })
+})
+
+app.post('/api/persons', async (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  try {
+    const isExist = await Person.exists({ name: body.name })
+
+    if (isExist) {
+      return response.status(400).json({
+        error: 'name must be unique'
+      })
+    } else {
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+
+      const result = await person.save()
+      response.json(result)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message,
+      name: error.name
+    })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+module.exports = app
